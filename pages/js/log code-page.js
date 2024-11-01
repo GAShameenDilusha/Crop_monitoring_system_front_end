c3e5a70b33fe374cbde8e7c5bd85e277e04f0e42
shameen <shameendilusha5678@gmail.com>
2024-11-01 16:42:47 +0530

    update log code-page.js

--- pages/js/log code-page.js
+++ pages/js/log code-page.js
@@ -280,3 +280,30 @@ function changeLogFiles() {
     $('#btn-log-save').css('display', 'block');
     clearLogFields();
 }
+
+
+
+
+//////////////////////////////////////////////////
+/*image start*/
+function previewFieldImage(input, previewId) {
+    const preview = document.getElementById(previewId);
+
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        preview.src = '../assets/img/no-image-img.jpg';
+    }
+}
+
+// To get the selected image if needed
+function getObservedImage() {
+    return document.getElementById('file-observed-image').files[0];
+}
+/*end*/
